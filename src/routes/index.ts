84e605c219fa73cedcddaa431b1aac4a38c6b414
JDGarzon <93989744+JDGarzon@users.noreply.github.com>
2024-03-10 14:34:43 -0500

    authorization to events

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,13 +11,13 @@ const routes = (app: Express) => {
     app.post('/users', validateSchema(userSchema), userController.create);
     app.put('/users/:id', userController.update );
     app.delete('/users/:id', userController.delete );
-    app.get('/event', eventController.getEvents);
-    app.post('/event', validateSchema(eventSchema), eventController.create);
-    app.put('/event/:id', eventController.update );
-    app.delete('/event/:id', eventController.delete );
+    app.get('/event',auth, eventController.getEvents);
+    app.post('/event', auth,validateSchema(eventSchema), eventController.create);
+    app.put('/event/:id', auth,eventController.update );
+    app.delete('/event/:id',auth, eventController.delete );
     app.get('/users/profile', auth, userController.findById);
     app.get('/users/:id', userController.findById);
-    app.get('/vent/:id', eventController.findById);
+    app.get('/vent/:id',auth, eventController.findById);
     app.post('/login/', userController.login);
 };
 
